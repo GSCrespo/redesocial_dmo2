5a1a68e6b2aab31ab48bfcee1f20b89e48c3a243
Guilherme <guilhermecresppo20@gmail.com>
2025-04-14 11:22:56 -0300

    Adicionando recyclerView para exibir posts

---
+++ app/src/main/java/com/ifsp/microredesocial/adapter/PostAdapter.kt
@@ -0,0 +1,32 @@
+package com.ifsp.microredesocial.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ifsp.microredesocial.R
+import com.ifsp.microredesocial.model.Post
+
+class PostAdapter (private val posts: Array<Post>) :
+    RecyclerView.Adapter<PostAdapter.ViewHolder>(){
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
+    {
+        val imgPost : ImageView = view.findViewById(R.id.imgPost)
+        val txtDescricao : TextView = view.findViewById(R.id.descricao)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.post_item, parent, false)
+        return ViewHolder(view)
+    }
+    override fun getItemCount(): Int {
+        return posts.size
+    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.txtDescricao.text = posts[position].getDescricao()
+        holder.imgPost.setImageBitmap(posts[position].getFoto())
+    }
+}
