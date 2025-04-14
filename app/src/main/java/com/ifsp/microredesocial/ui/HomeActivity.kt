d8e18b56bfaf67f077353e51e077db20245ae30d
Guilherme Crespo <guilhermecresppo20@gmail.com>
2025-04-13 22:53:38 -0300

    Enviando mudanças para HomeActivity (porem lidar com exceçao!)

--- app/src/main/java/com/ifsp/microredesocial/ui/HomeActivity.kt
+++ app/src/main/java/com/ifsp/microredesocial/ui/HomeActivity.kt
@@ -3,10 +3,13 @@ package com.ifsp.microredesocial.ui
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
 import com.ifsp.microredesocial.R
 import com.ifsp.microredesocial.databinding.ActivityHomeBinding
 import com.ifsp.microredesocial.databinding.ActivityLoginBinding
+import com.ifsp.microredesocial.util.Base64Converter
 
 class HomeActivity : AppCompatActivity() {
 
@@ -18,6 +21,22 @@ class HomeActivity : AppCompatActivity() {
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        val db = Firebase.firestore
+        val email = firebaseAuth.currentUser!!.email.toString()
+
+        db.collection("usuarios").document(email).get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful){
+                    val document = task.result
+                    val imageString = document.data!!["fotoPerfil"].toString()
+                    val bitmap = Base64Converter.stringToBitmap(imageString)
+                    binding.logoProfile.setImageBitmap(bitmap)
+                    binding.textUsername.text = document.data!!["username"].toString()
+                    binding.textNomeCompleto.text = document.data!!["nomeCompleto"].toString()
+                }
+            }
 
         setupListeners()
     }
